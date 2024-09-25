6b103bde6a2b4ca24406a922df5b4765230a91a0
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-09-25 18:27:08 -0300

    feat: add navbar style

---
+++ components/Navbar.tsx
@@ -0,0 +1,104 @@
+import {
+  HomeIcon,
+  LightBulbIcon,
+  LinkIcon,
+  QueueListIcon,
+  TableCellsIcon,
+} from "@heroicons/react/20/solid";
+import clsx from "clsx";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import {
+  ForwardRefExoticComponent,
+  RefAttributes,
+  SVGProps,
+  useEffect,
+  useState,
+} from "react";
+
+function Navbar() {
+  return (
+    <div className="w-[140px] lg:w-[220px] md:flex flex-col px-4 lg:px-8 py-4 hidden">
+      <a className="text-3xl mb-8">Gnars</a>
+      <div className="w-[220px] flex flex-col">
+        <a>MENU</a>
+        <div className="font-xl flex flex-col pt-4 gap-4">
+          <NavbarItem
+            href="/"
+            text="Discover"
+            icon={HomeIcon}
+            color="#CA6CFF"
+          />
+          <NavbarItem
+            href="/about"
+            text="About"
+            icon={LightBulbIcon}
+            color="#8FD1F9"
+          />
+          <NavbarItem
+            href="/vote"
+            text="Proposals"
+            icon={QueueListIcon}
+            color="#51DB6E"
+          />
+          <NavbarItem
+            href="/#"
+            text="Propdates"
+            icon={TableCellsIcon}
+            color="#CAEB00"
+          />
+          <NavbarItem
+            href="https://gnars.com"
+            text="Gnars"
+            icon={LinkIcon}
+            color="#EA8C3F"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+interface NavbarItemProps {
+  text: string;
+  href: string;
+  icon: ForwardRefExoticComponent<
+    SVGProps<SVGSVGElement> & RefAttributes<SVGSVGElement>
+  >;
+  className?: string;
+  color: string;
+}
+
+function NavbarItem(props: NavbarItemProps) {
+  const router = useRouter();
+  const [isHovered, setIsHovered] = useState(false);
+  const active = router.asPath === props.href || isHovered;
+
+  useEffect(() => {
+    setIsHovered(false);
+  }, []);
+
+  return (
+    <Link
+      href={props.href}
+      className={clsx("flex gap-2 items-center", props.className)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div
+        className={clsx(
+          "p-2 rounded-lg",
+          active
+            ? "bg-opacity-100 scale-105"
+            : "bg-gray-200 hover:bg-opacity-80"
+        )}
+        style={{ backgroundColor: active ? props.color : undefined }}
+      >
+        <props.icon width={"18px"} color={active ? "white" : "black"} />
+      </div>
+      <p>{props.text}</p>
+    </Link>
+  );
+}
+
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,7 @@
 import CustomConnectButton from "@/components/CustomConnectButton";
 import { useInitTheme } from "@/hooks/useInitTheme";
 import {
-  HomeIcon,
-  LightBulbIcon,
-  LinkIcon,
-  MagnifyingGlassIcon,
-  QueueListIcon,
-  TableCellsIcon,
+  MagnifyingGlassIcon
 } from "@heroicons/react/20/solid";
 import { lightTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import type { AppProps } from "next/app";
@@ -16,7 +11,7 @@ import { WagmiConfig } from "wagmi";
 import { chains, wagmiClient } from "../configs/wallet";
 import "../styles/globals.css";
 import tailwindConfig from "../tailwind.config.js";
-import Link from "next/link";
+import Navbar from "@/components/Navbar";
 
 const fullConfig = resolveConfig(tailwindConfig);
 const bg = (fullConfig.theme?.backgroundColor as any).skin;
@@ -41,48 +36,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           })}
         >
           <div className="bg-skin-backdrop font-body text-skin-base h-screen flex">
-            <div className="w-[140px] lg:w-[220px] md:flex flex-col px-4 lg:px-8 py-4 hidden">
-              <a className="text-3xl mb-8">Gnars</a>
-              <div className="w-[220px] flex flex-col">
-                <a>MENU</a>
-                <div className="font-xl flex flex-col pt-4 gap-4">
-                  <Link href={"/"} className="flex gap-2 items-center">
-                    <div className="p-2 rounded-lg bg-[#CA6CFF]">
-                      <HomeIcon width={"18px"} color="white" />
-                    </div>
-                    <a className="">Discover</a>
-                  </Link>
-                  <button className="flex gap-2 items-center group">
-                    <div className="p-2 bg-zinc-200 rounded-lg group-hover:bg-[#CA6CFF] group-hover:shadow-sm">
-                      <LinkIcon
-                        width={"18px"}
-                        color="black"
-                        className="color-white"
-                      />
-                    </div>
-                    <a className="">Gnars</a>
-                  </button>
-                  <button className="flex gap-2 items-center group">
-                    <div className="p-2 bg-zinc-200 rounded-lg group-hover:bg-[#CA6CFF] group-hover:shadow-sm">
-                      <LightBulbIcon width={"18px"} color="black" />
-                    </div>
-                    <a className="">About</a>
-                  </button>
-                  <Link href={"/vote"} className="flex gap-2 items-center group">
-                    <div className="p-2 bg-zinc-200 rounded-lg group-hover:bg-[#CA6CFF] group-hover:shadow-sm">
-                      <QueueListIcon width={"18px"} color="black" />
-                    </div>
-                    <a className="">Proposals</a>
-                  </Link>
-                  <button className="flex gap-2 items-center group">
-                    <div className="p-2 bg-zinc-200 rounded-lg group-hover:bg-[#CA6CFF] group-hover:shadow-sm">
-                      <TableCellsIcon width={"18px"} color="black" />
-                    </div>
-                    <a className="">Propdates</a>
-                  </button>
-                </div>
-              </div>
-            </div>
+            <Navbar />
             <div className="flex flex-col p-4 flex-grow h-full overflow-y-auto overflow-x-hidden">
               <div className="w-full pb-0 md:pb-4 flex justify-between gap-8">
                 <div className="flex w-full max-w-[450px] bg-gray-200 rounded-lg pr-2 items-center">
