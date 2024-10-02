a88954cf2d5d2467c898327b3aa7fc770862059a
r4to <r4topunk.eth@gmail.com>
2024-10-02 11:21:55 -0400

    fix: img to Image and remove hardflip

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Loading({ text }: { text?: string }) {
   return (
-    <div className="flex w-full h-full flex-col justify-center items-center animate-spin">
+    <div className="flex w-full h-full flex-col justify-center items-center">
       {text && (
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-4xl lg:text-2xl font-semibold tracking-tight text-black">
@@ -10,7 +11,7 @@ export default function Loading({ text }: { text?: string }) {
           </h1>
         </div>
       )}
-      <img src="/sktloading.gif" width={500} height={500} className="object-cover scale-[0.2]" />
+      <Image alt="image" src="/sktloading.gif" width={500} height={500} className="object-cover scale-[0.2]" />
     </div>
   );
 }
