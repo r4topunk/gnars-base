d2abebdbaaf19844371e10b92c3c095e315a5eba
r4to <r4topunk.eth@gmail.com>
2024-10-06 21:11:04 -0300

    feat: propdates news section (#4)
    * propdates dummy section
    
    * propdates dummy section text position
    
    * propdates dummy section style
    
    ---------
    
    Co-authored-by: sktbrd <sktbrd.eth@gmail.com>

--- components/News/NewsCard.tsx
+++ components/News/NewsCard.tsx
@@ -1,27 +1,29 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import Loading from "../Loading";
+// components/NewsCard.tsx
+import React from "react";
+import NewsItem from "./NewsItem"; // Adjust the path as needed
 
-// Enhanced Dummy News Data with varied content types
 const DummyNews = [
     {
-        title: "Rodrigo Panajotti Onboarding Proposal",
+        title: "Rodrigo Panajotti in the House",
         description: "Final updates from Rodrigo's onboarding proposal.",
-        type: "Proposal Update", // Differentiate the content type
-        thumbnail: "https://hackmd.io/_uploads/B10FNss3R.jpg",
+        type: "Proposal Update",
+        thumbnail: "https://ipfs.skatehive.app/ipfs/QmXeMcdcL1HuGYgJugdx8veh3nLYopEKkhEaqhBkKVLiz6",
         updates: 1,
         proposerAvatar: "https://i.ibb.co/ChbsDSr/Screenshot-1.png",
         proposer: "skateboard.⌐◨-◨",
+        proposalId: "1",
+        updateIndex: "1",
     },
     {
-        title: "SKATE AND JAZZ - 1st Noggles Rail in Argentina",
+        title: "Noggles Rail Phase 1",
         description: "First Noggles project in Argentina featuring skate and jazz.",
         type: "Proposal Update",
-        thumbnail:
-            "https://ipfs.skatehive.app/ipfs/QmWDuVTKkk58NCfpyLVLu4DT9tYKf87zNZixUygTt35PbC",
+        thumbnail: "https://ipfs.skatehive.app/ipfs/QmWDuVTKkk58NCfpyLVLu4DT9tYKf87zNZixUygTt35PbC",
         updates: 4,
-        proposerAvatar: "https://i.ibb.co/ChbsDSr/Screenshot-1.png",
-        proposer: "skateboard.⌐◨-◨",
+        proposerAvatar: "https://images.ecency.com/webp/u/karao7a/avatar/small",
+        proposer: "karao7a.eth",
+        proposalId: "2",
+        updateIndex: "1",
     },
     {
         title: "Top 10 Skate Spots in NYC",
@@ -29,17 +31,21 @@ const DummyNews = [
         type: "Blog Post",
         thumbnail: "https://gnars.com/images/93ciofh34f.jpeg",
         updates: 0,
-        proposerAvatar: "https://i.ibb.co/F76Q1J8/image.png",
-        proposer: "skaterank.eth",
+        proposerAvatar: "https://images.ecency.com/webp/u/willdias/avatar/small",
+        proposer: "willdias.eth",
+        proposalId: "3",
+        updateIndex: "1",
     },
     {
-        title: "Gnars Wakeboarding Culture",
+        title: "Gnars  Culture",
         description: "A new trend in wakeboarding driven by Gnars DAO.",
         type: "Proposal Update",
-        thumbnail: "https://hackmd.io/_uploads/S1cQRssqA.jpg",
+        thumbnail: "https://ipfs.skatehive.app/ipfs/QmQ14XPfkHR86huXJ8U8bebbb5Nb9dQrufANh9WndaKbhT",
         updates: 2,
-        proposerAvatar: "https://i.ibb.co/F76Q1J8/image.png",
-        proposer: "overhau1.eth",
+        proposerAvatar: "https://images.ecency.com/webp/u/joaoparmagnani/avatar/small",
+        proposer: "zimardrp.eth",
+        proposalId: "4",
+        updateIndex: "1",
     },
     {
         title: "How to Land a Kickflip",
@@ -47,8 +53,10 @@ const DummyNews = [
         type: "Blog Post",
         thumbnail: "https://gateway.pinata.cloud/ipfs/QmWUzfArhZVvyDjCBhEiQkAF5Q6npwYhKS5eMXWq1akqco",
         updates: 0,
-        proposerAvatar: "https://i.ibb.co/F76Q1J8/image.png",
-        proposer: "skateblogger.eth",
+        proposerAvatar: "https://euc.li/gami.eth",
+        proposer: "gami.eth",
+        proposalId: "5",
+        updateIndex: "1",
     },
     {
         title: "KnowHow is Hot",
@@ -58,6 +66,8 @@ const DummyNews = [
         updates: 0,
         proposerAvatar: "https://i.ibb.co/ChbsDSr/Screenshot-1.png",
         proposer: "skateboard.⌐◨-◨",
+        proposalId: "6",
+        updateIndex: "1",
     },
 ];
 
@@ -67,87 +77,12 @@ const NewsCard = () => {
             {DummyNews.map((news, index) => (
                 <div
                     key={index}
-                    className={`rounded-xl overflow-hidden shadow-lg ${index % 3 === 0
-                        ? "bg-lime-300"
-                        : index % 3 === 1
-                            ? "bg-red-300"
-                            : "bg-blue-300"
+                    className={`rounded-xl overflow-hidden shadow-lg ${index % 3 === 0 ? "bg-lime-300" : index % 3 === 1 ? "bg-red-300" : "bg-blue-300"
                         }`}
                 >
                     <NewsItem news={news} />
                 </div>
             ))}
-        </div>
-    );
-};
-
-interface NewsItemProps {
-    news: {
-        title: string;
-        description: string;
-        type: string;
-        thumbnail: string;
-        updates: number;
-        proposerAvatar: string;
-        proposer: string;
-    };
-}
-
-const NewsItem = ({ news }: NewsItemProps) => {
-    const [imageLoaded, setImageLoaded] = useState(false);
-
-    return (
-        <div className="relative rounded-lg overflow-hidden h-full">
-            {/* Image Container */}
-            <div
-                className={`${imageLoaded ? "hidden" : "absolute"
-                    } inset-0 flex justify-center items-center`}
-            >
-                <Loading />
-            </div>
-            <Image
-                onLoad={() => setImageLoaded(true)}
-                className="w-full h-32 lg:h-full object-cover"
-                src={news.thumbnail}
-                alt="news thumbnail"
-                width={400}
-                height={200}
-            />
-
-            {/* Overlay Container */}
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-between p-2 lg:p-4">
-                {/* Title and Content Type */}
-                <div>
-                    <p className="text-sm lg:text-lg font-semibold text-white">
-                        {news.title}
-                    </p>
-                    {/* <span className="text-xs lg:text-sm text-gray-300 italic">
-                        {news.type}
-                    </span> */}
-                </div>
-
-                {/* Proposer Information and Badge */}
-                <div className="flex justify-between items-center">
-                    <div className="flex items-center mb-2">
-                        <Image
-                            src={news.proposerAvatar}
-                            alt={`${news.proposer} avatar`}
-                            className="w-6 h-6 lg:w-8 lg:h-8 rounded-full object-cover"
-                            width={32}
-                            height={32}
-                        />
-                        <p className="ml-2 text-xs lg:text-sm text-white">{news.proposer}</p>
-                    </div>
-
-                    {/* Button based on Type */}
-                    {/* <button className={`px-2 py-1 rounded-lg ${news.type === "Proposal Update"
-                        ? "bg-yellow-500 text-black"
-                        : "bg-purple-500 text-white"
-                        }`}>
-                        {news.type === "Proposal Update" ? "Check Update" : "Read Post"}
-                    </button> */}
-                </div>
-            </div>
         </div>
     );
 };
---
+++ components/News/NewsItem.tsx
@@ -0,0 +1,84 @@
+// components/NewsItem.tsx
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Loading from "../Loading"; // Adjust the path to your Loading component if necessary.
+import { title } from "process";
+import { width, height } from "tailwindcss/defaultTheme";
+
+interface NewsItemProps {
+    news: {
+        title: string;
+        description: string;
+        type: string;
+        thumbnail: string;
+        updates: number;
+        proposerAvatar: string;
+        proposer: string;
+        proposalId?: string;
+        updateIndex?: string;
+    };
+}
+
+const NewsItem = ({ news }: NewsItemProps) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    return (
+        <Link href={news.type === "Blog Post" ? `/blog/${news.proposalId}` : `/propdates/${news.proposalId}/${news.updateIndex}`}>
+            <div className="relative rounded-lg overflow-hidden h-full group ">
+                {/* Enhanced Ribbon for Proposal Updates */}
+                {/* {news.type === "Proposal Update" && (
+                    <div className="absolute top-10 left-0 bg-gradient-to-r from-purple-500 to-purple-700 text-white px-4 py-1 transform -rotate-45 origin-top-left z-20 shadow-md rounded-tr-lg">
+                        <span className="font-bold text-md">propdate</span>
+                    </div>
+                )} */}
+
+                {/* Image Container */}
+                <div className={`${imageLoaded ? "hidden" : "absolute"} inset-0 flex justify-center items-center`}>
+                    <Loading />
+                </div>
+
+                <Image
+                    onLoad={() => setImageLoaded(true)}
+                    className="w-full h-32 lg:h-full object-cover group-hover:scale-105 transition-transform duration-300"
+                    src={news.thumbnail}
+                    alt="news thumbnail"
+                    width={400}
+                    height={200}
+                />
+
+                {/* Overlay Container */}
+                <div className="absolute inset-0 flex flex-col justify-between p-1 lg:p-2  group-hover:bg-opacity-0 group-hover:backdrop-blur-none transition duration-300  bg-black bg-opacity-50 ">
+                    <div className="flex justify-center items-center mt-5">
+                        {/* <Image
+                            src={news.proposerAvatar}
+                            alt={`${news.proposer} avatar`}
+                            className="w-12 h-12 lg:w-16 lg:h-16 rounded-full object-cover"
+                            width={64}
+                            height={64}
+                        /> */}
+                    </div>
+                    {/* Title and Content Type */}
+                    <div className="bg-opacity-0 group-hover:bg-yellow-400 group-hover:bg-opacity-100 rounded-lg p-1 lg:p-2 flex items-center space-x-2 lg:space-x-4 h-12 lg:h-12 transition duration-300">
+                        <Image
+                            src={news.proposerAvatar}
+                            alt={`${news.proposer} avatar`}
+                            className="w-8 h-8 rounded-full object-cover"
+                            width={8}
+                            height={8}
+                        />
+                        <p className="text-sm lg:text-md text-white group-hover:text-black font-normal max-w-full transition duration-300">
+                            {news.title}
+                        </p>
+                    </div>
+                    {/* <div className="group-hover:hidden text-sm lg:text-md text-black font-normal max-w-full">
+                        {news.title}
+                    </div> */}
+
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default NewsItem;
