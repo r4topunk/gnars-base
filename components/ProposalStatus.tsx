75ed0ac805d038049da3baa3766f33acb8f75f46
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-10-22 20:51:23 -0300

    style: update the transaction colors again

--- components/ProposalStatus.tsx
+++ components/ProposalStatus.tsx
@@ -7,55 +7,55 @@ export default function ProposalStatus({ proposal }: { proposal: Proposal }) {
   switch (state) {
     case 0:
       return (
-        <div className="bg-gray-400 text-fill p-1 px-2 rounded-md w-24 text-center">
+        <div className="bg-gray-100 text-gray-700 p-1 px-2 rounded-md w-24 text-center">
           Pending
         </div>
       );
     case 1:
       return (
-        <div className="bg-green-400 text-fill p-1 px-2 rounded-md w-24 text-center">
+        <div className="bg-green-100 text-green-700 p-1 px-2 rounded-md w-24 text-center">
           Active
         </div>
       );
     case 2:
       return (
-        <div className="bg-rose-400 text-fill p-1 px-2 rounded-md w-24 text-center">
+        <div className="bg-red-100 text-red-700 p-1 px-2 rounded-md w-24 text-center">
           Canceled
         </div>
       );
     case 3:
       return (
-        <div className="bg-red-400 text-fill p-1 px-2 rounded-md w-24 text-center">
+        <div className="bg-red-100 text-red-700 p-1 px-2 rounded-md w-24 text-center">
           Defeated
         </div>
       );
     case 4:
       return (
-        <div className="bg-green-400 text-fill p-1 px-2 rounded-md w-24 text-center">
+        <div className="bg-green-100 text-green-700 p-1 px-2 rounded-md w-24 text-center">
           Succeeded
         </div>
       );
     case 5:
       return (
-        <div className="bg-purple-400 text-fill p-1 px-2 rounded-md w-24 text-center">
+        <div className="bg-blue-100 text-blue-700 p-1 px-2 rounded-md w-24 text-center">
           Queued
         </div>
       );
     case 6:
       return (
-        <div className="bg-gray-400 text-fill p-1 px-2 rounded-md w-24 text-center">
+        <div className="bg-gray-100 text-gray-700 p-1 px-2 rounded-md w-24 text-center">
           Expired
         </div>
       );
     case 7:
       return (
-        <div className="bg-blue-400 text-fill p-1 px-2 rounded-md w-24 text-center">
+        <div className="bg-purple-100 text-purple-700 p-1 px-2 rounded-md w-24 text-center">
           Executed
         </div>
       );
     case 8:
       return (
-        <div className="bg-gray-400 text-fill p-1 px-2 rounded-md w-24 text-center">
+        <div className="bg-gray-100 text-gray-700 p-1 px-2 rounded-md w-24 text-center">
           Vetoed
         </div>
       );
